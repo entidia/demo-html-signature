4c8edb9faa8b847669f841dee73455ccccb5e3a2
Markus Deuerlein <mdeuerlein@users.noreply.github.com>
2018-03-01 04:31:15 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,17 @@
-var wrapper = document.getElementById("signature-pad");
-var clearButton = wrapper.querySelector("[data-action=clear]");
-var changeColorButton = wrapper.querySelector("[data-action=change-color]");
-var undoButton = wrapper.querySelector("[data-action=undo]");
-var savePNGButton = wrapper.querySelector("[data-action=save-png]");
-var saveJPGButton = wrapper.querySelector("[data-action=save-jpg]");
-var saveSVGButton = wrapper.querySelector("[data-action=save-svg]");
-var canvas = wrapper.querySelector("canvas");
-var signaturePad = new SignaturePad(canvas, {
-  // It's Necessary to use an opaque color when saving image as JPEG;
-  // this option can be omitted if only saving as PNG or SVG
-  backgroundColor: 'rgb(255, 255, 255)'
-});
+var wrapper           = document.getElementById("signature-pad");
+var clearButton       = wrapper.querySelector("[data-action=clear]");
+var savePNGButton     = wrapper.querySelector("[data-action=save-png]");
+var canvas            = wrapper.querySelector("canvas");
+var signaturePad      = new SignaturePad(canvas, { backgroundColor: 'rgb(255, 255, 255)'});
 
-// Adjust canvas coordinate space taking into account pixel ratio,
-// to make it look crisp on mobile devices.
-// This also causes canvas to be cleared.
 function resizeCanvas() {
-  // When zoomed out to less than 100%, for some very strange reason,
-  // some browsers report devicePixelRatio as less than 1
-  // and only part of the canvas is cleared then.
   var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-
-  // This part causes the canvas to be cleared
   canvas.width = canvas.offsetWidth * ratio;
   canvas.height = canvas.offsetHeight * ratio;
   canvas.getContext("2d").scale(ratio, ratio);
-
-  // This library does not listen for canvas changes, so after the canvas is automatically
-  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-  // that the state of this library is consistent with visual state of the canvas, you
-  // have to clear it manually.
   signaturePad.clear();
 }
 
-// On mobile devices it might make more sense to listen to orientation change,
-// rather than window resize events.
 window.onresize = resizeCanvas;
 resizeCanvas();
 
@@ -54,8 +30,6 @@ function download(dataURL, filename) {
   window.URL.revokeObjectURL(url);
 }
 
-// One could simply use Canvas#toBlob method instead, but it's just to show
-// that it can be done using result of SignaturePad#toDataURL.
 function dataURLToBlob(dataURL) {
   // Code taken from https://github.com/ebidel/filer.js
   var parts = dataURL.split(';base64,');
@@ -83,6 +57,4 @@ savePNGButton.addEventListener("click", function (event) {
     var dataURL = signaturePad.toDataURL().replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
     document.getElementById('downloadlink').innerHTML += '<a href="'+dataURL+'" download="signature.png" class="dwl_link">DOWNLOAD</a>';
   }
-
-
 });
